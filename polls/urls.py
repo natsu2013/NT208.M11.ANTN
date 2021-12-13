f95741b6a13a2046fb43658f1d35f64c88b1dff3
TT <54493212+Tuan164@users.noreply.github.com>
2021-12-13 10:05:11 +0700

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    #path('blog', views.blog, name='blog'),
-    # path('feedback', views.feedback, name='feedback'),
     path('contact', views.handleFeedback, name='contact'),
     path('menu/<int:_id>', views.BlogDetailView, name='menu'),
 ]
